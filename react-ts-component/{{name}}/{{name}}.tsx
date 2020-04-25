808a1ba5b613c780e8730288d4dea08040fff829
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-04-25 23:26:25 +0300

    initial commit

---
+++ react-ts-component/{{name}}/{{name}}.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface {{name}}Props {
+
+}
+
+const {{name}}: React.SFC<{{name}}Props> = (props) => {
+    const {
+
+    } = props;
+
+    return (
+        <div className="{{name}}"></div>
+    );
+};
+
+export { {{name}} };
